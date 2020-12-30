b0e7181853c53a226a137437342dd4ccf3f2894e
rajp19081 <rajp1908@gmail.com>
2020-12-30 03:28:44 -0500

    modified spacy file updated

--- raj_code/sentence_categorization/using_spacy.py
+++ raj_code/sentence_categorization/using_spacy.py
@@ -8,11 +8,11 @@ import spacy
 # spacy.download("en")
 nlp = spacy.load("en_core_web_md")
 
-df = pd.read_csv("B00CHTPGIS.csv")
+df = pd.read_csv("aspects_B002XTCAXG.csv")
 reviews = str(df["reviews"].iloc[4])
 
 lem = WordNetLemmatizer()
-
+#sentences = nlp(reviews)
 text_tokens = word_tokenize(reviews)
 all_stopwords = stopwords.words('english')
 all_stopwords.append([",", ".", "//", ":", ";", "'"])
@@ -46,20 +46,42 @@ colour, color, hotspot, spill, corona, lux, candelas, intensity, lights"
 price_keywords_string = "Price, cost, costly, pricey, pricy, expensive, overpriced, reasonable,\
 unreasonable, value, affordable, cheap, $, bargain, budget, cash, discount, money, sale"
 
-list1 = "size durable battery beam price"
-# size_keywords = size_keywords_string.lower().replace(" ","").split(",")
-# quality_keywords = quality_keywords_string.lower().replace(" ","").split(",")
-# battery_keywords = battery_keywords_string.lower().replace(" ","").split(",")
-# design_keywords = design_keywords_string.lower().replace(" ","").split(",")
-# beam_keywords = beam_keywords_string.lower().replace(" ","").split(",")
-# price_keywords = price_keywords_string.lower().replace(" ","").split(",")
+list1 = "size. quality. battery life. beam. price."
+size_keywords = size_keywords_string.lower().replace(" ","").split(",")
+quality_keywords = quality_keywords_string.lower().replace(" ","").split(",")
+battery_keywords = battery_keywords_string.lower().replace(" ","").split(",")
+design_keywords = design_keywords_string.lower().replace(" ","").split(",")
+beam_keywords = beam_keywords_string.lower().replace(" ","").split(",")
+price_keywords = price_keywords_string.lower().replace(" ","").split(",")
 
-# list1 = size_keywords + quality_keywords + battery_keywords + design_keywords + beam_keywords + price_keywords
-
+#print(size_keywords)
+#list1 = " ".join(size_keywords) + " ".join(quality_keywords) + " ".join(battery_keywords) + " ".join(design_keywords) + " ".join(beam_keywords) + " ".join(price_keywords)
+# unigram
 tokens = nlp(filtered_sentence)
 tokens1 = nlp(list1)
-for token in tokens:
-    for tkn in tokens1:
-        if (token and token.vector_norm) and (tkn and tkn.vector_norm):
-            if round(token.similarity(tkn), 2) >= 0.40:
-                print(token.text, tkn.text, token.similarity(tkn))
+#for token in tokens:
+#    for tkn in tokens1:
+#        if (token and token.vector_norm) and (tkn and tkn.vector_norm):
+#            if round(token.similarity(tkn), 2) >= 0.40:
+#                print(token.text, tkn.text, token.similarity(tkn))
+
+
+# sentences
+df1 = None
+for index, review in enumerate(df["reviews"]):
+    dic = {"reviews":[]}
+    sentences = nlp(review)
+    for token in sentences.sents:  # reviews
+        for tkn in tokens1.sents:  # defined categories
+            dic["reviews"].append(review)
+            if (token and token.vector_norm) and (tkn and tkn.vector_norm):
+                if round(token.similarity(tkn), 2) >= 0.60:  # can be tweaked further to modify accuracy of results
+                    if dic.get(tkn.text):
+                        dic[tkn.text].append(token.text)
+                    else:
+                        dic[tkn.text] = [token.text]
+                    print(f'{token.text} - {tkn.text}, {token.similarity(tkn)}')
+    frame = pd.DataFrame.from_records([dic])
+    df1 = frame if df1 is None else pd.concat([df1, frame])
+df1.set_index(df["reviews"], inplace=True)
+df1.to_csv("spacy_op_6threshold.csv")
